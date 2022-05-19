56cf169b82779d5db104e77561d1e7ab853129ae
kkparashar78@gmail.com <kkparashar78@gmail.com>
2022-05-19 12:44:56 +0530

    env and procfile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const parikshaRoutes = require("./routes/pariksha");
 const parikshaAdmin = require("./routes/admin");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
