5ffc97fd57ee82db6e457011899ecc598acaf1ba
kkparashar78@gmail.com <kkparashar78@gmail.com>
2022-05-22 17:15:56 +0530

    CORS added

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const parikshaRoutes = require("./routes/pariksha");
 const parikshaAdmin = require("./routes/admin");
 const port = process.env.PORT || 9000;
 
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
